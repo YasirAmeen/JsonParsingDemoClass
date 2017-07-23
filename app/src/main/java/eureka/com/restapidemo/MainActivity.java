87e9e18b83e141edb9813f494078f25b9112baf7
Yasir Ameen <YasirAmeen92@gmail.com>
2017-07-23 13:47:14 +0500

    asdf

--- app/src/main/java/eureka/com/restapidemo/MainActivity.java
+++ app/src/main/java/eureka/com/restapidemo/MainActivity.java
@@ -1,16 +1,29 @@
 package eureka.com.restapidemo;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
+
+import net.alhazmy13.mediapicker.Image.ImagePicker;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -22,13 +35,18 @@ public class MainActivity extends AppCompatActivity {
     EditText _password;
     Button _btn;
 
+    ImageView _img;
+    private String imagepath;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        _img = (ImageView) findViewById(R.id.imageView2);
 
-        AsyncHttpClient client = new AsyncHttpClient();
+
+      /*  AsyncHttpClient client = new AsyncHttpClient();
         client.post(MainActivity.this, "https://blistered-groans.000webhostapp.com/test/select.php",null, new AsyncHttpResponseHandler() {
 
 
@@ -66,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
                 pd.dismiss();
 
             }
-        });
+        });*/
 
        //=================================================================================
 
@@ -86,10 +104,68 @@ public class MainActivity extends AppCompatActivity {
         });*/
 
 
+      //Parsing Json Data
+
+        String json = loadJSONFromAsset();
+
+        try {
+            JSONObject object = new JSONObject(json);
+            JSONArray data = object.getJSONArray("data");
+
+            for (int i = 0; i < data.length(); i++) {
+
+                JSONObject ob = data.getJSONObject(i);
+
+                JSONArray xyz = ob.getJSONArray("actions");
+
+                String msg = ob.getString("message");
+
+                JSONObject from = data.getJSONObject(i).getJSONObject("from");
+                String name = from.getString("name");
+
+
+                Toast.makeText(this, ""+msg+ " " +name, Toast.LENGTH_SHORT).show();
+            }
+
+
+
+            //Log.d("json",object.toString());
+            Log.d("data",data.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new ImagePicker.Builder(MainActivity.this)
+                        .mode(ImagePicker.Mode.GALLERY)
+                        .compressLevel(ImagePicker.ComperesLevel.MEDIUM)
+                        .directory(ImagePicker.Directory.DEFAULT)
+                        .scale(600, 600)
+                        .allowMultipleImages(false)
+                        .enableDebuggingMode(true)
+                        .build();
+            }
+        });
 
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == ImagePicker.IMAGE_PICKER_REQUEST_CODE && resultCode == RESULT_OK) {
+            List<String> mPaths = (List<String>) data.getSerializableExtra(ImagePicker.EXTRA_IMAGE_PATH);
+
+             imagepath = mPaths.get(0).trim();
+            Toast.makeText(this, ""+imagepath, Toast.LENGTH_SHORT).show();
+
+            //Your Code
+        }
+    }
 
     private void UserSignup(String name, String email, String password) {
 
@@ -139,5 +215,22 @@ public class MainActivity extends AppCompatActivity {
                 pd.dismiss();
             }
         });
+    }
+
+
+    public String loadJSONFromAsset() {
+        String json = null;
+        try {
+            InputStream is = getAssets().open("facebook.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
     }
 }
